ec0d61e587ddcf8cae594324deed9eb3221b4023
Marta <martavivesluis@gmail.com>
2018-01-04 21:56:18 +0100

    iniciant creacio nou personatge

--- app/src/main/java/edu/upc/dsa/clientjoc/NewPersonaje.java
+++ app/src/main/java/edu/upc/dsa/clientjoc/NewPersonaje.java
@@ -1,10 +1,60 @@
 package edu.upc.dsa.clientjoc;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import edu.upc.dsa.beans.Personatge;
 
 /**
  * Created by Marta on 4/1/2018.
  */
 
 public class NewPersonaje extends AppCompatActivity {
+    public ImageView imageType1view;
+    public ImageView imageType2view;
+    public ImageView imageType3view;
+    public ImageView imageType4view;
+    public TextView nameText;
+    public Personatge created;
+    public void associatedViews(){
+        imageType1view = (ImageView)findViewById(R.id.imageViewtype1);
+        imageType2view = (ImageView)findViewById(R.id.imageViewType2);
+        imageType3view = (ImageView)findViewById(R.id.imageViewType3);
+        imageType4view = (ImageView)findViewById(R.id.imageViewType4);
+        nameText = (TextView)findViewById(R.id.textView13);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newpersonaje);
+        associatedViews();
+        created = new Personatge();
+        imageType1view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                created.setTipo(1);
+
+         }});
+        imageType2view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                created.setTipo(2);
+            }});
+        imageType3view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                created.setTipo(3);
+            }});
+        imageType4view.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                created.setTipo(4);
+            }});
+
+    }
+    private boolean isNameValid(String name) {
+        return name.length() > 4;
+    }
+    private void attemptCreating(){
+
+    }
 }
